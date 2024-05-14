1dd03d61ffc13b5602e2f772094ecb73ef233fb0
somelunastuff <luvilarsen@gmail.com>
2024-05-14 19:54:30 +0200

    added increment

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // document.getElementById("count-el").innerText = 5
 
+let countEl = document.getElementById("count-el")
+
 let count = 0
 
-console.log(count)
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
